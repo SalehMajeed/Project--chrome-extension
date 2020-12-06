f54f24f584777270306afcfd4eaf5aef7c1e5ac1
= <saleh.majeed8@gmail.com>
2020-12-06 19:06:28 +0530

    chrome extension

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+(function () {
+	let script = document.createElement('script');
+	// script.src = 'http://127.0.0.1:5500/chrome%20Extension/bookmarklet.js';
+	document.body.appendChild(script);
+})();
---
+++ content.js
@@ -0,0 +1,7 @@
+console.log('Chrome extension go?');
+
+let paragraphs = document.getElementsByTagName('p');
+
+for (elt of paragraphs) {
+	elt.style['background-color'] = 'black';
+}
